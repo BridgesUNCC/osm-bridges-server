ef43d312ce5f7af036700370625e18eaf5a9edd9
Jason (Jay) Strahler <jason.strahler@gmail.com>
2019-06-17 16:03:28 -0400

    Sever setup files

---
+++ initialize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo python3.6 -m pip install flask
+sudo python3.6 -m pip install wget
+sudo python3.6 -m pip install apscheduler
+
+python3.6 -m venv venv
+source venv/bin/activate
---
+++ python_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum -y install yum-utils
+sudo yum -y groupinstall development
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum -y install python36u
+sudo yum -y install python36u-pip
+sudo yum -y install python36u-devel
