1fc88d0f8d7a607f4cb7689923451a67b1997c14
Erik Saule <esaule@uncc.edu>
2020-08-01 20:22:15 -0400

    python setup update

--- python_setup.sh
+++ python_setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+PYTHON=python3
+
 sudo yum -y install yum-utils
 sudo yum -y groupinstall development
 sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum -y install python36u
 sudo yum -y install python36u-pip
 sudo yum -y install python36u-devel
-sudo python3.6 -m pip install flask
-sudo python3.6 -m pip install wget
-sudo python3.6 -m pip install apscheduler
-sudo python3.6 -m pip install virtualenv
+sudo ${PYTHON} -m pip install flask
+sudo ${PYTHON} -m pip install wget
+sudo ${PYTHON} -m pip install apscheduler
+sudo ${PYTHON} -m pip install virtualenv
